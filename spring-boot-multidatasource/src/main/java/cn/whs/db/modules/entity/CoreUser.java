cf4c7509d65eec9bbf529c321562c8eeaa521abe
wuhaisheng <wuhaisheng@binggou.com>
2018-03-29 14:43:13 +0800

    spring-boot-jwt 添加 @Data注解

--- spring-boot-multidatasource/src/main/java/cn/whs/db/modules/entity/CoreUser.java
+++ spring-boot-multidatasource/src/main/java/cn/whs/db/modules/entity/CoreUser.java
@@ -3,6 +3,8 @@ package cn.whs.db.modules.entity;
 import com.baomidou.mybatisplus.activerecord.Model;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.enums.IdType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -13,8 +15,8 @@ import java.time.LocalDateTime;
  * @description
  * @date 2018-03-28 15:58
  */
-//@EqualsAndHashCode(callSuper = false)
-//@Data
+@EqualsAndHashCode(callSuper = false)
+@Data
 public class CoreUser  extends Model {
     private static final long serialVersionUID = 1L;
 
@@ -36,54 +38,6 @@ public class CoreUser  extends Model {
         this.mobile = mobile;
         this.password = password;
         this.insertTime = LocalDateTime.now();
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDateTime getInsertTime() {
-        return insertTime;
-    }
-
-    public void setInsertTime(LocalDateTime insertTime) {
-        this.insertTime = insertTime;
     }
 
     @Override
