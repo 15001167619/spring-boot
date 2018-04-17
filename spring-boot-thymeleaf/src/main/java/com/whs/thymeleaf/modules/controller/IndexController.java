5fae92c2dfdb08e7f293090def76e25082e89652
wuhaisheng <wuhaisheng@binggou.com>
2018-04-17 16:27:34 +0800

    spring-boot

--- spring-boot-thymeleaf/src/main/java/com/whs/thymeleaf/modules/controller/IndexController.java
+++ spring-boot-thymeleaf/src/main/java/com/whs/thymeleaf/modules/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.whs.thymeleaf.modules.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("msg", "welcome you!");
         return "index";
     }
 
